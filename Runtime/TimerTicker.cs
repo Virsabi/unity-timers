0cc5ac7a6d10a6ad401b3fd99360ae9bd47ce25c
Mathias Munk Ulrich <mu@virsabi.com>
2020-12-21 11:39:09 +0100

    unscaled time implemented

--- Runtime/TimerTicker.cs
+++ Runtime/TimerTicker.cs
@@ -2,17 +2,27 @@ using System;
 using System.Collections;
 using UnityEngine;
 
-namespace ElRaccoone.Timers {
-  public class TimerTicker : MonoBehaviour {
-    public IEnumerator EnumerateSetTimeout (int miliseconds, Action callback) {
-      yield return new WaitForSeconds (miliseconds / 1000);
-      callback ();
-    }
+namespace ElRaccoone.Timers
+{
+    public class TimerTicker : MonoBehaviour
+    {
+        public IEnumerator EnumerateSetTimeout(int miliseconds, Action callback, bool unscaledTime)
+        {
+            if (unscaledTime)
+                yield return new WaitForSecondsRealtime(miliseconds / 1000);
+            else
+                yield return new WaitForSeconds(miliseconds / 1000);
+            callback();
+        }
 
-    public IEnumerator EnumerateSetInterval (int miliseconds, Action callback) {
-      yield return new WaitForSeconds (miliseconds / 1000);
-      callback ();
-      this.StartCoroutine (this.EnumerateSetInterval (miliseconds, callback));
+        public IEnumerator EnumerateSetInterval(int miliseconds, Action callback, bool unscaledTime)
+        {
+            if (unscaledTime)
+                yield return new WaitForSecondsRealtime(miliseconds / 1000);
+            else
+                yield return new WaitForSeconds(miliseconds / 1000);
+            callback();
+            this.StartCoroutine(this.EnumerateSetInterval(miliseconds, callback, unscaledTime));
+        }
     }
-  }
 }
--- Runtime/Timers.cs
+++ Runtime/Timers.cs
@@ -15,28 +15,28 @@ namespace ElRaccoone.Timers {
       }
     }
 
-    public static void SetTimeout (int miliseconds, Action callback) {
-      Enumerator.StartCoroutine (Enumerator.EnumerateSetTimeout (miliseconds, callback));
+    public static void SetTimeout (int miliseconds, Action callback, bool unscaledTime) {
+      Enumerator.StartCoroutine (Enumerator.EnumerateSetTimeout (miliseconds, callback, unscaledTime));
     }
 
-    public static void SetInterval (int miliseconds, Action callback) {
-      Enumerator.StartCoroutine (Enumerator.EnumerateSetInterval (miliseconds, callback));
+    public static void SetInterval (int miliseconds, Action callback, bool unscaledTime) {
+      Enumerator.StartCoroutine (Enumerator.EnumerateSetInterval (miliseconds, callback, unscaledTime));
     }
 
-    public static void SetTimeout (this Transform target, int miliseconds, Action callback) {
-      Enumerator.StartCoroutine (Enumerator.EnumerateSetTimeout (miliseconds, callback));
+    public static void SetTimeout (this Transform target, int miliseconds, Action callback, bool unscaledTime) {
+      Enumerator.StartCoroutine (Enumerator.EnumerateSetTimeout (miliseconds, callback, unscaledTime));
     }
 
-    public static void SetInterval (this Transform target, int miliseconds, Action callback) {
-      Enumerator.StartCoroutine (Enumerator.EnumerateSetInterval (miliseconds, callback));
+    public static void SetInterval (this Transform target, int miliseconds, Action callback, bool unscaledTime) {
+      Enumerator.StartCoroutine (Enumerator.EnumerateSetInterval (miliseconds, callback, unscaledTime));
     }
 
-    public static void SetTimeout (this GameObject target, int miliseconds, Action callback) {
-      Enumerator.StartCoroutine (Enumerator.EnumerateSetTimeout (miliseconds, callback));
+    public static void SetTimeout (this GameObject target, int miliseconds, Action callback, bool unscaledTime) {
+      Enumerator.StartCoroutine (Enumerator.EnumerateSetTimeout (miliseconds, callback, unscaledTime));
     }
 
-    public static void SetInterval (this GameObject target, int miliseconds, Action callback) {
-      Enumerator.StartCoroutine (Enumerator.EnumerateSetInterval (miliseconds, callback));
+    public static void SetInterval (this GameObject target, int miliseconds, Action callback, bool unscaledTime) {
+      Enumerator.StartCoroutine (Enumerator.EnumerateSetInterval (miliseconds, callback, unscaledTime));
     }
   }
 }
